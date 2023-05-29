8939b62d6a0af65f5e21eb464ec8023ca91bd691
adilleilani <maryumsaleem390@gmail.com>
2023-05-29 02:47:41 -0700

    added validations in UserSchema

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 const UserScehma = mongoose.Schema({
-    name:String,
-    password: String,
-    email: String
+    name: {
+        type: String,
+        required: [true, "Please tell your name"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide your email address"]
+    }
 });
 
 //a model is a class with which we construct documents
